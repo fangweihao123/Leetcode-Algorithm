7a0d41de779983206d921475ccf48d6a426e85e7
po <137012120@qq.com>
2020-02-11 14:51:19 +0800

    update

---
+++ 55-跳跃游戏/回溯法.cpp
@@ -0,0 +1,53 @@
+#include<vector>
+#include<string>
+#include<iostream>
+#include<stack>
+using namespace std;
+
+//判断是否可以跳到终点
+enum INDEX{
+    UNKNOWN,
+    BAD,
+    GOOD
+};
+
+class Solution {
+private:
+    int* record;
+public:
+    bool dfs(vector<int>& nums,int index){
+        if(index==nums.size()-1)
+            return true;
+        if(index>nums.size()-1)
+            return false;
+        for(int i=1;i<=nums[index];i++){
+            if(record[index+i] == 0){
+                if(dfs(nums,index+i)){
+                    record[index] = 2;
+                    return true;
+                }
+            }else if(record[index+i] == 1){
+                continue;
+            }else if(record[index+i] == 2){
+                return true;
+            }
+        }
+        record[index] = 1;
+        return false;
+    }
+
+    bool canJump(vector<int>& nums) {
+        record = new int[nums.size()];
+        for(int i=0;i<nums.size();i++)
+            record[i] = 0;
+        return dfs(nums,0);
+    }
+};
+
+int main(){
+    Solution slu;
+    vector<int> nums = {0,1};
+    bool flag = slu.canJump(nums);
+    cout<<flag<<endl;
+    return 0;
+}
